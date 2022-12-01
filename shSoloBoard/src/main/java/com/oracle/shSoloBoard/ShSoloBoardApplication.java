b05234528c6e083e82c3b95fc365ebb41b22fde2
holyflower95 <holyflower95@naver.com>
2022-12-01 17:39:30 +0900

    세팅

---
+++ shSoloBoard/src/main/java/com/oracle/shSoloBoard/ShSoloBoardApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.shSoloBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShSoloBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShSoloBoardApplication.class, args);
+	}
+
+}
---
+++ shSoloBoard/src/test/java/com/oracle/shSoloBoard/ShSoloBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.shSoloBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShSoloBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
